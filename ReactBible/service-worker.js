eea2926839d41e225eaf87b719d455486df01de7
suenting <kurosuen@gmail.com>
2018-09-26 18:05:21 -0700

    UpdateUI/LocalStorage

--- ReactBible/service-worker.js
+++ ReactBible/service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
 var precacheConfig = [
-        ["./index.html", "d520a434f81fa204557ff5ccc337df1b"],
-        ["./static/js/main.789ba063.js", "0b0b10fc1cb5b573ca04fe9feb884a28"],
+        ["./index.html", "1d32ec35bfd7b4d559f17143adb823fc"],
+        ["./static/js/main.acba379e.js", "0b0b10fc1cb5b573ca04fe9feb884b28"]
         ["./static/css/main.b6382374.css", "0b0b10fc1cb5b573ca04fe9feb884a17"]
     ],
     cacheName = "sw-precache-v3-sw-precache-webpack-plugin-" + (self.registration ? self.registration.scope : ""),
