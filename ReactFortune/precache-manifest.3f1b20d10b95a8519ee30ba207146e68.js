689e49f42b3790df13eb97395d5a9eaa6fb31cc4
suenting <kurosuen@gmail.com>
2018-11-04 22:21:12 -0800

    add react fortune testpage

---
+++ ReactFortune/precache-manifest.3f1b20d10b95a8519ee30ba207146e68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "40ec3bafcd2c3704cdce",
+    "url": "/static/js/main.40ec3baf.chunk.js"
+  },
+  {
+    "revision": "0ad5b2d2ebb41810c315",
+    "url": "/static/js/1.0ad5b2d2.chunk.js"
+  },
+  {
+    "revision": "40ec3bafcd2c3704cdce",
+    "url": "/static/css/main.eda844a5.chunk.css"
+  },
+  {
+    "revision": "3ea4602c4798cf053fe2928de1209cdf",
+    "url": "/index.html"
+  }
+];
--- webserver.py
+++ webserver.py
@@ -1,21 +1,10 @@
-import sys
-import BaseHTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
+import http.server
+import socketserver
 
+PORT = 8000
 
-HandlerClass = SimpleHTTPRequestHandler
-ServerClass  = BaseHTTPServer.HTTPServer
-Protocol     = "HTTP/1.0"
+Handler = http.server.SimpleHTTPRequestHandler
 
-if sys.argv[1:]:
-    port = int(sys.argv[1])
-else:
-    port = 8000
-server_address = ('127.0.0.1', port)
-
-HandlerClass.protocol_version = Protocol
-httpd = ServerClass(server_address, HandlerClass)
-
-sa = httpd.socket.getsockname()
-print "Serving HTTP on", sa[0], "port", sa[1], "..."
-httpd.serve_forever()
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
