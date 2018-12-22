e68b93c78e8890b07f55b43959b0b371e53fda2b
suenting <kurosuen@gmail.com>
2018-12-22 11:22:40 -0800

    added togetherjs test

--- ReactBible/service-worker.js
+++ ReactBible/service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
 var precacheConfig = [
-        ["./index.html", "1d32ec35bfd7b4d559f17143adb823fc"],
-        ["./static/js/main.acba379e.js", "0b0b10fc1cb5b573ca04fe9feb884b28"],
+        ["./index.html", "681b09ebd95366f6a2d7d18646c831e4"],
+        ["./static/js/main.298699f0.js", "0b0b10fc1cb5b573ca04fe9feb884a18"],
         ["./static/css/main.b6382374.css", "0b0b10fc1cb5b573ca04fe9feb884a17"]
     ],
     cacheName = "sw-precache-v3-sw-precache-webpack-plugin-" + (self.registration ? self.registration.scope : ""),
