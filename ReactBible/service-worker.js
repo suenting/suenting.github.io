f68414e6ce5c85230dbf920dbe7efd11ff8ada51
suenting <kurosuen@gmail.com>
2018-12-27 10:12:33 -0800

    fix duplicate key issue

--- ReactBible/service-worker.js
+++ ReactBible/service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
 var precacheConfig = [
-        ["./index.html", "f2016e5bd73c84f98bc2ad174089d3e8"],
-        ["./static/js/main.3dcb7577.js", "f2016e5bd73c84f98bc2ad174089d3e9"],
+        ["./index.html", "cdc4d7834d71022002d40cf7143a5ff0"],
+        ["./static/js/main.f1dd7df2.js", "0b0b10fc1cb5b573ca04fe9feb884a18"]
         ["./static/css/main.b6382374.css", "0b0b10fc1cb5b573ca04fe9feb884a17"]
     ],
     cacheName = "sw-precache-v3-sw-precache-webpack-plugin-" + (self.registration ? self.registration.scope : ""),
