fc385571a8c7c6ab1b058892ac0fa861794047bb
suenting <kurosuen@gmail.com>
2026-05-14 11:06:22 -0700

    update bible build

--- ReactBible/service-worker.js
+++ ReactBible/service-worker.js
@@ -1,8 +1,8 @@
 "use strict";
 var precacheConfig = [
-        ["./index.html", "e88c6964"],
-        ["./static/css/main.15c457d6.css", "15c457d6"],
-        ["./static/js/main.e88c6964.js", "e88c6964"]
+        ["./index.html", "225ab80e"],
+        ["./static/css/main.fe5f3ebf.css", "fe5f3ebf"],
+        ["./static/js/main.225ab80e.js", "225ab80e"]
     ],
     cacheName = "sw-precache-v3-sw-precache-webpack-plugin-" + (self.registration ? self.registration.scope : ""),
     ignoreUrlParametersMatching = [/^utm_/],
